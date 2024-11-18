c1758e2b4e76ce969c9b8d7a8fb7f3159c3aa27e
MukarramHaq <mukarram796@gmail.com>
2024-11-17 20:47:29 -0500

    Somehow completed the mobile layout. Its probably a very bad example but its okay

---
+++ index.js
@@ -0,0 +1,14 @@
+// const radioBtnEl = document.getElementById("radio-button");
+// const imageEl = document.getElementById("img-one");
+// console.log(radioBtnEl);
+// console.log(imageEl);
+
+// imageEl.addEventListener("click", () => {
+//     imageEl.style.display= "none";
+//     radioBtnEl.style.display = "block";
+// })
+
+// radioBtnEl.addEventListener("click", () => {
+//     radioBtnEl.style.display = "none";
+//     imageEl.style.display = "block";
+// })
